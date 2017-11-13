d684ac61f84d9c63cd0e7b86921523a484b1c683
Samovar34 <alexkh34@gmail.com>
2017-11-13 17:33:46 +0300

    add phaser

--- src/js/game.js
+++ src/js/game.js
@@ -1,1 +1,37 @@
-var game = new Phaser.Game(640, 480, Phaser.AUTO, "");
+//@@include('./options.js')
+
+var game;
+
+window.onload = function () {
+    game = new Phaser.Game(640, 480, Phaser.AUTO);
+    game.state.add("PreloadGame", preloadGame);
+    //game.state.add("PlayGame", playGame);
+    game.state.start("PreloadGame");
+};
+function preloadGame(game) {};
+preloadGame.prototype = {
+    preload: function () {
+        // setup game area
+        game.stage.backgroundColor = gameOptions.bgColor;
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        game.scale.disableVisibilityChange = true;
+        
+        // loading level tilemap and player image
+        game.load.tilemap("level", "/assets/levels/level.json", Phaser.Tilemap.TILED_JSON);
+        game.load.image("tile", "/assets/images/tile.png");
+        game.load.image("player", "/assets/images/player.png");
+    },
+    create: function () {
+        console.log("create");
+        //game.state.start("PlayGame");
+    }
+};
+
+function playGame (game) {};
+playGame.prototype = {
+    preload: function () {
+        
+    }
+}
---
+++ src/js/options.js
@@ -0,0 +1,12 @@
+var gameOptions = {
+    gameWidth: 640,
+    gameHeight: 480,
+    bgColor: 0x444444,
+    player : {
+        gravity: 900,
+        grip: 100,
+        speed: 200,
+        jump: 400,
+        doubleJump: 300
+    }
+};
