e0c97a7bab1836ab21d382f64128ae53d28f9451
Samovar34 <alexkh34@gmail.com>
2017-12-06 22:11:28 +0300

    add bacis movement

--- src/js/game.js
+++ src/js/game.js
@@ -2,10 +2,7 @@
 
 var game;
 var player;
-
-var canvasWidth = 320;
-var canvasHeight = 240;
-var resizeRatio = 1;
+var cursors;
 
 window.onload = function () {
     // 480x270
@@ -18,6 +15,7 @@ window.onload = function () {
     game.state.add("PlayGame", playGame);
     game.state.start("PreloadGame");
 };
+
 var preloadGame =  function (game) {};
 preloadGame.prototype = {
     preload: function () {
@@ -28,33 +26,105 @@ preloadGame.prototype = {
         game.renderer.renderSession.roundPixels = true;
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         //game.scale.disableVisibilityChange = true;
-        
+
+        // load assets
+        game.load.tilemap('map', 'assets/levels/layout1.csv', null, Phaser.Tilemap.CSV);
+        game.load.image('tiles', 'assets/images/tilemap.png');
+        game.load.image("player", "/assets/images/player.png");
 
     },
     create: function () {
-        console.log("create");
-        console.log("preload", resizeRatio);
         game.state.start("PlayGame");
     }
 };
 
 var playGame = function (game) {};
 playGame.prototype = {
-    preload: function () {
-        game.load.tilemap('map', 'assets/levels/layout1.csv', null, Phaser.Tilemap.CSV);
-        game.load.image('tiles', 'assets/images/tilemap.png');
-        game.load.image("player", "/assets/images/player.png");
-    },
-
+    // main state functions
     create: function () {
-        var map = game.add.tilemap("map", 16, 16);
-        map.addTilesetImage("tiles");
-        var layer = map.createLayer(0);
-        layer.resizeWorld();
+        // state variables
+        this.canJump = true;
+        this.canDoubleJump = false;
+        this.onWall = false; // player on wall (true/false)
+
+        // game objects
+        this.map = game.add.tilemap("map", 16, 16);
+        this.map.addTilesetImage("tiles");
+        this.map.setCollision([0, 1, 2, 3, 4, 5, 6, 7]);
+        
+        this.layer = this.map.createLayer(0);
+        this.layer.resizeWorld();
+
+        // player
         player = game.add.sprite(80, 80, "player");
         player.anchor.setTo(0.5);
-        player.scale.setTo(0.7);
+        player.scale.setTo(0.5);
         player.smoothed = false;
+        
+        // enable physics for the player
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        player.body.gravity.y = gameOptions.player.gravity;
+        player.body.velocity.x = gameOptions.player.speed;
 
+        // enable input
+        cursors = game.input.keyboard.createCursorKeys();
+    },
+
+    update: function () {
+        // reset values
+        this.setDefaultValues();
+        var self = this;
+
+        // physics
+        game.physics.arcade.collide(player, this.layer, function (p, tile) {
+            // if player hit something, no double jump is allowed
+            self.canDoubleJump = false;
+            
+            if (p.body.blocked.down) {
+                self.canJump = true;
+            }
+        });
+
+        // handle input
+        // TODO не равнозначный ввод left right
+        if (cursors.left.isDown) {
+            player.body.velocity.x = gameOptions.player.speed * -1;
+        }
+
+        if (cursors.right.isDown) {
+            player.body.velocity.x = gameOptions.player.speed;
+        }
+
+        // jump without checking
+        if (cursors.up.justDown) {
+            this.handleJump();
+        }
+    },
+
+    // custom state functions
+    setDefaultValues: function () {
+        player.body.velocity.x = 0;
+    },
+
+    setPlayerXVelocity: function (defaultDirection) {
+        
+    },
+
+    handleJump: function () {
+        console.log("DO");
+        if ((this.canJump && player.body.blocked.down) || this.onWall) {
+            console.log("FIRST");
+            player.body.velocity.y= gameOptions.player.jump * -1;
+
+            // запретить обычный прыжок и разрешить двойной
+            this.canJump = false;
+            this.canDoubleJump = true;
+        } else {
+            if (this.canDoubleJump) {
+                console.log("DOUBLE");
+                this.canDoubleJump = false;
+                player.body.velocity.y = gameOptions.player.doubleJump * -1;
+            }
+        }
     }
 };
--- src/js/options.js
+++ src/js/options.js
@@ -1,12 +1,12 @@
 var gameOptions = {
     gameWidth: 640,
     gameHeight: 480,
-    bgColor: 0x444444,
+    bgColor: 0x001199,
     player : {
         gravity: 900,
         grip: 100,
-        speed: 200,
-        jump: 400,
+        speed: 150,
+        jump: 300,
         doubleJump: 300
     }
 };
