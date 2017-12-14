22180afe81685d6cc896b0ae5d2fbcaeee29fb79
Samovar34 <alexkh34@gmail.com>
2017-12-14 10:01:11 +0300

    player class

--- src/js/options.js
+++ src/js/options.js
@@ -1,13 +1,13 @@
 var gameOptions = {
     gameWidth: 1280,
     gameHeight: 720,
-    bgColor: 0x001199,
+    bgColor: 0x001155,
     player : {
         gravity: 900,
         grip: 50,
         speed: 500,
         runSpeed: 650,
-        jump: 400,
-        doubleJump: 300
+        jump: 500,
+        doubleJump: 400
     }
 };
--- src/js/player.js
+++ src/js/player.js
@@ -1,2 +1,13 @@
 // Player class
+var Player = function (game, x, y) {
+	Phaser.Sprite.call(this, game, x, y, "player");
+	
+	this.game = game;
+	
+	this.anchor.setTo(0.5);
+	player.smoothed = false;
+	
+	//add to game
+	this.game.add.existing(this);
+}
 
